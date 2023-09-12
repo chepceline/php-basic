b8a6dac51d9e9e9a2a9afc61835459c9c0d8f147
Celine <chepceline25@gmail.com>
2023-09-13 00:40:34 +0300

    write variable notes

---
+++ basic.php
@@ -0,0 +1,16 @@
+<?php
+/* what is a variable
+
+    A variable is a like a container for storing data
+
+    PHP variables
+
+    A variable can have a short name like (x and y) or a more descriptive name (age, carname,total_volume)
+
+    Rules for PHP variables
+
+    1. A variable starts with the $ sign, followed by the name of the variable
+    2. A variable name must start with a letter or the underscore character
+    3. A variable name cannot start with a number
+    4. A variable name can only contain alpha-numeric characters and underscores
+    5. Variable names are case-sensitive 
