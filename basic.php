69520e2b0faa47e2a520964a77a8e776521439a4
Celine <chepceline25@gmail.com>
2023-09-13 12:27:23 +0300

    create a php program to calculate score

--- basic.php
+++ basic.php
@@ -13,4 +13,38 @@
     2. A variable name must start with a letter or the underscore character
     3. A variable name cannot start with a number
     4. A variable name can only contain alpha-numeric characters and underscores
-    5. Variable names are case-sensitive 
+    5. Variable names are case-sensitive
+
+     write a php program that will print the grades based on the scores above
+
+91-100=A
+81-90=B
+71-80=C
+61-70=D
+51-60=6
+50 and below = F */
+
+
+$score=69;
+
+if ($score<=50){
+    echo "F grade";
+}
+else if ($score>=51 and $score<=60){
+    echo "E grade";
+}
+else if ($score>=61 and $score<=70){
+    echo "D grade";
+}
+else if ($score>=71 and $score<=80){
+    echo "C grade";
+}
+else if ($score>=81 and $score<=90){
+    echo "B grade";
+}
+else if ($score>=91 and $score<=100){
+    echo "A grade";
+}
+else {
+    echo "invalid input";
+}
